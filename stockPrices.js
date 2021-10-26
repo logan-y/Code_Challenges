a1d451749363408efb59d7441b2165084fb3e525
Logan Young <logany@att.net>
2021-10-26 10:34:59 -0500

    stockPrices challenge uploaded 10/26

---
+++ stockPrices.js
@@ -0,0 +1,42 @@
+// const best = (arr) => {
+//     let sellPrice = 0;
+//     // let buyPrice = Math.min(...arr);
+//     arr.filter(price => {
+//         if(price > sellPrice){
+//             return sellPrice = price
+//         }
+//     })
+
+//    return sellPrice;
+// }
+
+// console.log(best([2, 4, 7, 8, 3, 9, 3, 5, 1]));
+
+
+const best = (arr) => {
+    let sellPrice = 0
+    let sellPriceIndex = 0
+
+    arr.forEach((i, el) => {
+        if (i > sellPrice){
+            sellPrice = i
+            sellPriceIndex = el
+        }
+    })
+    let buyPrice = sellPrice
+
+    for (let i = 0; i < sellPriceIndex; i++){
+        if (arr[i] < buyPrice){
+            buyPrice = arr[i];
+        }
+    }
+    if(sellPrice <= buyPrice){
+        return 0 
+    }
+    return `Buy at ${buyPrice}, sell at ${sellPrice}. Your profit will be ${sellPrice - buyPrice}`
+}
+    
+
+
+ console.log(best([3, 4, 7, 8, 2, 9, 3, 5, 1]));
+
